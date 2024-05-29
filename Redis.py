4f31b15155c206cc0a9f4d3eb751c6be5e6fd3c8
Rohaan Malik <rohaan@nextgenbt.com>
2024-05-29 12:24:19 -0400

    Added the basic set, get & delete functionality to redis

--- Redis.py
+++ Redis.py
@@ -1,4 +1,34 @@
+import time
 class MiniRedis:
     def __init__(self):
         self.store = {}
         self.expiration = {}
+    
+    def set(self, key, value):
+        """ Stores the key-value pair in the database"""
+        with self.lock:
+            self.store[key] = value
+            if key in self.expiration:
+                del self.expiration[key]
+            return "OK"
+
+    def get(self, key):
+        """ Retrieves the value associated with the key."""
+        with self.lock:
+            if key in self.expiration and time.time() > self.expiration[key]:
+                del self.store[key]
+                del self.expiration[key]
+                return None
+            if key in self.store:
+                return self.store[key]
+            return None
+    
+    def delete(self, key):
+        """ Deletes the key-value pair from the database."""
+        with self.lock:
+            if key in self.store:
+                del self.store[key]
+                if key in self.expiration:
+                    del self.expiration[key]
+                return 1
+            return 0
